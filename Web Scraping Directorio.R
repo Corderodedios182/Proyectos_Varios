591afbea7abf123d4bb436a71afeb87043ac6144
Charly1046 <Corderodedios182@gmail.com>
2018-05-25 17:33:00 -0500

    Add files via upload

--- Web Scraping Directorio.R
+++ Web Scraping Directorio.R
@@ -34,3 +34,27 @@ Listado <- rbind(Listado,tabla)
 #Siguientes pasos;
  #-Separar los datos de la columna uno y dejar especificadas las columnas.
  #-Revisar la inforamcion de las imagenes.
+
+library(stringr)
+#Especificamos los valores por los que queremos separar
+Columnas <- c('Departamento:', ' Cargo:', 'Teléfono:', 'Fax:', 'Intercom:', 'Ubicación:')
+
+#Base para Separarla Inormacion
+test_0 <- Listado ; colnames(test_0) <- c('X2','page')
+
+#For que separa los datos de acuerdo al Vector Columnas
+for(i in 1:6){
+  eval(parse(text = paste0( 'test_',i,' <- data.frame(str_split_fixed(test_',i-1,'$X2,"',Columnas[i],'",2))')))
+  }
+
+#Creacion de la Base
+Listado_F <- data.frame(Profesor = test_1$X1,
+                        Departamento = test_2$X1,
+                        Cargo = test_3$X1,
+                        Telefono = test_4$X1,
+                        Fax = test_5$X1,
+                        Intercom = test_6$X1,
+                        Ubicacion = test_6$X2)
+
+rm(list=ls(pattern="^test"))
+
