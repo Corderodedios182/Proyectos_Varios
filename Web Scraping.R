e22635dfde1e1247c30f1d657284fd853414fb04
Charly1046 <Corderodedios182@gmail.com>
2018-06-16 20:45:52 -0500

    Add files via upload

--- Web Scraping.R
+++ Web Scraping.R
@@ -1,8 +1,19 @@
 rm(list = ls())
 #El siguiente script extaera los datos que se encuentran en el Directorio de la pagina (http://www.fisica.unam.mx/)
 #Se busca guardar las fotografias y datos.
-library(rvest) ; library(tidyverse) ; library(stringr)
-#Paso 0
+library(rvest) ; library(tidyverse) ; library(stringr) ; require(magick) 
+
+#C:\Users\Finanzas\Downloads\Proyectos_SS-master\Directorio
+Ruta <- readline("Introduce una Ruta Donde se guardaran las Publicaciones y las Fotos : ") 
+setwd(Ruta)
+dir()
+
+Fotos <- as.character(readline("Introduce el Nombre de una Carpeta para guardar solo las Fotos:"))
+Fotos_Direccion <- dir.create(Fotos)
+
+Fotos <- paste0(Ruta,'\\',Fotos)
+
+#Paso 0 (Se descarga la Informacion de la estructura HTML)
 url <- "http://w2.fisica.unam.mx/directory/info/search_researchers?page=1"
 url_html <- read_html(url) #Lemos la info html
 
@@ -31,7 +42,9 @@ colnames(tabla) <- c("info","page")
 Listado <- rbind(Listado,tabla)
 }
 
-  #-Separar los datos de la columna uno y dejar especificadas las columnas.
+#########################################
+#Limpieza y Estructura para el DataFrame#
+#########################################
 
 #Especificamos los valores por los que queremos separar
 Columnas <- c('Departamento:', ' Cargo:', 'Teléfono:', 'Fax:', 'Intercom:', 'Ubicación:')
@@ -45,10 +58,11 @@ for(i in 1:6){
   }
 
 #Creacion de la Base
-Listado_F <- data.frame(Profesor = test_1$X1,
+Listado_F <- data.frame(Page = Listado$page,
+                        Profesor = test_1$X1,
                         Departamento = test_2$X1,
                         Cargo = test_3$X1,
-                        Telefono = test_4$X1,
+                        Telefono = trimws(test_4$X1),
                         Fax = test_5$X1,
                         Intercom = test_6$X1,
                         Ubicacion = test_6$X2)
@@ -57,14 +71,19 @@ rm(list=ls(pattern="^test"))
 
 Listado_F <- separate(Listado_F, col = Profesor, into = c("Prof","Grado"), sep = ",") 
 
+Listado_F$Prof <- trimws(Listado_F$Prof)
+Listado_F$Grado <- trimws(Listado_F$Grado)
+
 Listado_F <- unite(Listado_F, Profesor, Grado, Prof, sep = " ")
 
-Listado_F$Departamento <- data.frame(a = str_replace(Listado_F$Departamento,",",""))
+Listado_F$Departamento <- str_replace(Listado_F$Departamento,",","")
 
-Listado_F$Fax <- data.frame(a =str_replace(Listado_F$Fax,",",""))
+Listado_F$Fax <- str_replace(Listado_F$Fax,",","")
 
-  #Imagenes
-  #-Revisar la inforamcion de las imagenes.
+##########
+#Imagenes#
+##########
+#Se descarga la Url de las Imagenes
 
 url <- "http://w2.fisica.unam.mx/directory/info/search_researchers?page=1"
 url_html <- read_html(url) #Lemos la info html
@@ -107,4 +126,39 @@ Tabla_Final <- cbind(Listado_F,Imagen_url = f[,"Imagen_Url"])
 
 rm(f,f_4,img_1,Listado,Listado_F,tabla,tabla_0)
 
+Tabla_Final <- data.frame(Tabla_Final)
+
+Test <- Tabla_Final$Profesor
+Test <- data.frame(Profesor = trimws(strsplit(Test,split = " ")))
+Test$Profesor <- gsub("\"","",Test$Profesor)
+Test$Profesor <- gsub(",","",Test$Profesor)
+Test$Profesor <- gsub("c\\(","",Test$Profesor)
+Test$Profesor <- gsub(")","",Test$Profesor)
+
+Tabla_Final$Profesor <- Test$Profesor
+
+write.csv(Tabla_Final,"Tabla_final.csv",row.names = FALSE)
+
+#Descarga de las Imagenes a una carpeta
+setwd(Fotos)
+
+Profesor <- paste0(Tabla_Final[,1]," ",Tabla_Final[,2]) #Fijos
+links <- Tabla_Final[,9] #Fijos
+
+for(i in 1:40){
+eval(parse(text = paste0('Imagen_',i,' <- image_read(as.character(links[',i,']))'))) # Ciclo
+eval(parse(text = paste0('image_write(Imagen_',i,',paste0(as.character(Profesor[',i,']),".jpg"))'))) #Ciclo
+eval(parse(text = paste0('rm(Imagen_',i,')')))
+}
+
+#No se abre la conexion y no puedo descargar esta imagen :(
+#Imagen_41 <- image_read(as.character(links[41]),strip = 1) # Ciclo
+#image_write(Imagen_41,paste0(as.character(Profesor[41]),".jpg"))
+#print(Imagen_41)  
+
+for(i in 42:129){
+  eval(parse(text = paste0('Imagen_',i,' <- image_read(as.character(links[',i,']))'))) # Ciclo
+  eval(parse(text = paste0('image_write(Imagen_',i,',paste0(as.character(Profesor[',i,']),".jpg"))'))) #Ciclo
+  eval(parse(text = paste0('rm(Imagen_',i,')')))
+}
 
